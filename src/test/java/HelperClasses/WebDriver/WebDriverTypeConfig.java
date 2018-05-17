ab1be74aa1aff105e6ca13710eed714cf9a94f53
Marcus Smith <marcus.sparky77@gmail.com>
2018-05-17 20:44:51 +0100

    Modified : Driver Location Folders, to include Mac & Windows. Added : Mac Chrome and PhantomJS Drivers. Updated WebDriverTypeConfig.java to independently handle Mac and Windows machines launching webdrivers.

--- src/test/java/HelperClasses/WebDriver/WebDriverTypeConfig.java
+++ src/test/java/HelperClasses/WebDriver/WebDriverTypeConfig.java
@@ -6,36 +6,60 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.io.File;
 
 public abstract class WebDriverTypeConfig {
-    public final String resourceDriverLocation = "/src/test/resources/drivers/";
-    public final String driverType = ReadFrom.propertiesFile("defaultSetupProperties","driverType");
+    private final String resourceDriverLocation = "/src/test/resources/drivers/";
+    private String modifiedOSDriverLocation;
+    final String driverType = ReadFrom.propertiesFile("defaultSetupProperties","driverType");
+    private String osType;
+    private File classPathRoot;
     public static WebDriver driver; // static driver so can be shared with other Pages and does not need instantiation
 
     protected WebDriver lauchDriverType(String driverType){
-        File classPathRoot = new File(System.getProperty("user.dir"));
-        String fullPathToDrivers = classPathRoot + resourceDriverLocation;
+        classPathRoot = new File(System.getProperty("user.dir"));
+        osType = getOSType();
         checkWebDriverUniqueInstance();
+
         switch(driverType.toLowerCase()) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", fullPathToDrivers + "chromedriver.exe");
-                driver = new ChromeDriver();
+                chromeDriverOSPathConstructor();
                 break;
             case "phantomjs":
-                DesiredCapabilities caps = new DesiredCapabilities();
-                caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, fullPathToDrivers + "phantomjs.exe");
-                driver = new PhantomJSDriver(caps);
+                phantomJSDriverOSPathConstructor();
                 break;
         }
         return driver;
     }
 
+    private void phantomJSDriverOSPathConstructor() {
+        if (osType.contains("Mac")) {
+            modifiedOSDriverLocation = classPathRoot + resourceDriverLocation + "Mac/phantomjs";
+        } else if (osType.contains("Windows")) {
+            modifiedOSDriverLocation = classPathRoot + resourceDriverLocation + "Win/phantomjs.exe";
+        }
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, modifiedOSDriverLocation);
+        driver = new PhantomJSDriver(caps);
+    }
+
+    private void chromeDriverOSPathConstructor() {
+        if (osType.contains("Mac")) {
+            modifiedOSDriverLocation = classPathRoot + resourceDriverLocation + "Mac/chromedriver";
+        } else if (osType.contains("Windows")) {
+            modifiedOSDriverLocation = classPathRoot + resourceDriverLocation + "/Win/chromedriver.exe";
+        }
+        System.setProperty("webdriver.chrome.driver", modifiedOSDriverLocation);
+        driver = new ChromeDriver();
+    }
+
+    private String getOSType(){
+        return System.getProperty("os.name");
+    }
+
     private void checkWebDriverUniqueInstance(){
         if (driver == null){ // singleton type patten - only want one instance of webDriver
-            System.out.println("Original Driver instance ... launching : " + driverType);
-            System.out.println(".........................");
+            System.out.println("\nOriginal Driver instance ... launching : " + driverType);
         } else {
             System.out.println("Cannot launch another driver instance - already assigned to : " + driverType);
         }
